37eeb1c87e849c1bc3aa134b4e4bc21537e78da7
Cyrill Schumacher <cyrill@schumacher.fm>
2014-08-31 19:32:17 +1000

    [PICNIC] add favicon, the gopher

--- picnic/handlers.go
+++ picnic/handlers.go
@@ -14,19 +14,34 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// Wanderlust uses go.rice package for serving static web content
+//
+// To embed web content, please download go.rice and run
+// `rice embed-go` in "picnic" directory and use the generated Go source code in your project.
+
 package picnic
 
 import (
 	"fmt"
 	"github.com/SchumacherFM/wanderlust/github.com/gorilla/mux"
+	rice "github.com/SchumacherFM/wanderlust/github.com/GeertJohan/go.rice"
 	"net/http"
 )
+
+func getRiceImgBox() *rice.Box {
+	return rice.MustFindBox(RD_DIST_DIR + "/img")
+}
 
 func getRoutes() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/", dashBoardHandler).Methods("GET")
+	router.HandleFunc("/favicon.ico", handlerFavicon)
 	router.HandleFunc("/test", testDataHandler).Methods("GET")
 	return router
+}
+
+func handlerFavicon(w http.ResponseWriter, r *http.Request) {
+	w.Write(getRiceImgBox().MustBytes("favicon.ico"))
 }
 
 func dashBoardHandler(w http.ResponseWriter, r *http.Request) {
