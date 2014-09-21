b501f0012b573321e135d0da5d9a7f39cdadc3ac
Cyrill Schumacher <cyrill@schumacher.fm>
2014-09-22 09:34:31 +1000

    Fixing import paths

--- github.com/juju/errgo/errors_test.go
+++ github.com/juju/errgo/errors_test.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"testing"
 
-	gc "launchpad.net/gocheck"
+	gc "github.com/SchumacherFM/wanderlust/github.com/go-check/check"
 
 	"github.com/SchumacherFM/wanderlust/github.com/juju/errgo"
 )
