f513e24d10efec2530b6ee306400e9cd04c9e119
Cyrill Schumacher <cyrill@schumacher.fm>
2014-09-24 09:16:45 +1000

    [picnic] Track anon user for piwik

--- picnic/rd/src/js/dashboard/controllers/login-ctrl.js
+++ picnic/rd/src/js/dashboard/controllers/login-ctrl.js
@@ -1,41 +1,44 @@
 angular
-  .module('Dashboard')
-  .controller('LoginCtrl', [
-    '$scope',
-    '$location',
-    '$window',
-    '$http',
-    'Session',
-    'AuthResource',
-    'Alert',
-    'AUTH_TOKEN_HEADER',
-    function ($scope,
-              $location,
-              $window,
-              $http,
-              Session,
-              AuthResource,
-              Alert,
-              AUTH_TOKEN_HEADER) {
+    .module('Dashboard')
+    .controller('LoginCtrl', [
+        '$scope',
+        '$location',
+        '$window',
+        '$http',
+        'Session',
+        'AuthResource',
+        'Alert',
+        'TrackUser',
+        'AUTH_TOKEN_HEADER',
+        function ($scope,
+                  $location,
+                  $window,
+                  $http,
+                  Session,
+                  AuthResource,
+                  Alert,
+                  TrackUser,
+                  AUTH_TOKEN_HEADER) {
 
-      $scope.formData = new AuthResource();
+            $scope.formData = new AuthResource();
 
-      $scope.login = function () {
-        $scope.formData.$save(function saveLoginPost(result, headers) {
-          $scope.formData = new AuthResource();
-          console.log(result, headers(AUTH_TOKEN_HEADER));
-          if (result.loggedIn) {
-            Session.login(result, headers(AUTH_TOKEN_HEADER));
-            Alert.success("Welcome back, " + result.name);
-            var path = Session.getLastLoginUrl();
-            console.log(path);
-            //if (path) {
-            //  $location.path(path);
-            //} else {
-            //  $window.history.back();
-            //}
-          }
-        });
-      };
-    }
-  ]);
+            $scope.login = function () {
+                $scope.formData.$save(function saveLoginPost(result, headers) {
+                    $scope.formData = new AuthResource();
+                    console.log(result,headers, headers(AUTH_TOKEN_HEADER));
+                    if (result.loggedIn) {
+                        Session.login(result, headers(AUTH_TOKEN_HEADER));
+                        Alert.success("Welcome back, " + result.name);
+                        var path = Session.getLastLoginUrl();
+
+                        TrackUser.setUser(result.userName)
+                        //if (path) {
+                        //  $location.path(path);
+                        //} else {
+                        //  $window.history.back();
+                        //}
+                    }
+                });
+            };
+        }
+    ]);
--- picnic/rd/src/js/dashboard/services.js
+++ picnic/rd/src/js/dashboard/services.js
@@ -1,151 +1,167 @@
 /* Services */
 angular.module('picnic.services', [])
-  .service('Session', ['$location', '$window', '$q', 'AUTH_TOKEN_STORAGE_KEY', 'Alert',
-    function ($location, $window, $q, AUTH_TOKEN_STORAGE_KEY, Alert) {
-      var noRedirectUrls = {
-        "/login": true,
-        "/changepass": true,
-        "/recoverpass": true
-      };
+    .service('TrackUser', ['$window', 'md5',
+        function ($window, md5) {
+            var setUser = function (user) {
+                if (_paq in $window) {
+                    $window._paq.push(['setCustomVariable', // piwik specific
+                        1, // Index, the number from 1 to 5 where this custom variable name is stored
+                        "Username",
+                        md5(user),
+                        "visit" // scope
+                    ]);
+                }
+            };
+            return {
+                "setUser": setUser
+            }
+        }])
+    .service('Session', ['$location', '$window', '$q', 'AUTH_TOKEN_STORAGE_KEY', 'Alert',
+        function ($location, $window, $q, AUTH_TOKEN_STORAGE_KEY, Alert) {
+            var noRedirectUrls = {
+                "/login": true,
+                "/changepass": true,
+                "/recoverpass": true
+            };
 
-      function isNoRedirectFromLogin(url) {
-        return noRedirectUrls[url] || false;
-      }
+            function isNoRedirectFromLogin(url) {
+                return noRedirectUrls[url] || false;
+            }
 
-      function Session() {
-        this.clear();
-        this.lastLoginUrl = null;
-      }
+            function Session() {
+                this.clear();
+                this.lastLoginUrl = null;
+            }
 
-      Session.prototype.init = function (authResource) {
-        this.authResource = authResource;
-        this.sync();
-      };
+            Session.prototype.init = function (authResource) {
+                this.authResource = authResource;
+                this.sync();
+            };
 
-      Session.prototype.sync = function () {
-        var $this = this,
-            d = $q.defer();
-        $this.authResource.get({}, function (result) {
-          $this.login(result);
-          d.resolve(result);
-        });
-        return d.promise;
-      };
+            Session.prototype.sync = function () {
+                var $this = this,
+                    d = $q.defer();
+                $this.authResource.get({}, function (result) {
+                    $this.login(result);
+                    d.resolve(result);
+                });
+                return d.promise;
+            };
 
-      Session.prototype.redirectToLogin = function () {
-        this.clear();
-        this.setLastLoginUrl();
-        Alert.danger("You must be logged in");
-        $location.path("/login");
-      };
+            Session.prototype.redirectToLogin = function () {
+                this.clear();
+                this.setLastLoginUrl();
+                Alert.danger("You must be logged in");
+                $location.path("/login");
+            };
 
-      Session.prototype.check = function () {
-        var $this = this;
-        $this.sync().then(function () {
-          if (!$this.loggedIn) {
-            $this.redirectToLogin();
-          }
-        });
-      };
+            Session.prototype.check = function () {
+                var $this = this;
+                $this.sync().then(function () {
+                    if (!$this.loggedIn) {
+                        $this.redirectToLogin();
+                    }
+                });
+            };
 
-      Session.prototype.setLastLoginUrl = function () {
-        this.lastLoginUrl = $location.path();
-      };
+            Session.prototype.setLastLoginUrl = function () {
+                this.lastLoginUrl = $location.path();
+            };
 
-      Session.prototype.getLastLoginUrl = function () {
-        var url = this.lastLoginUrl;
-        if (true === isNoRedirectFromLogin(url)) {
-          url = null;
+            Session.prototype.getLastLoginUrl = function () {
+                var url = this.lastLoginUrl;
+                if (true === isNoRedirectFromLogin(url)) {
+                    url = null;
+                }
+                this.lastLoginUrl = null;
+                return url;
+            };
+
+            Session.prototype.clear = function () {
+                this.loggedIn = false;
+                this.name = null;
+                this.userName = null;
+                this.email = null;
+                this.id = null;
+                this.isAdmin = false;
+            };
+
+            Session.prototype.set = function (session) {
+                this.loggedIn = session.loggedIn;
+                this.name = session.name;
+                this.userName = session.userName;
+                this.email = session.email;
+                this.id = session.id;
+                this.isAdmin = session.isAdmin;
+            };
+
+            Session.prototype.login = function (result, token) {
+                this.set(result);
+                this.$delete = result.$delete;
+                if (token) {
+                    $window.localStorage.setItem(AUTH_TOKEN_STORAGE_KEY, token);
+                }
+            };
+
+            Session.prototype.logout = function () {
+                var $this = this,
+                    d = $q.defer();
+                $this.$delete(function (result) {
+                    $this.clear();
+                    d.resolve(result);
+                    $window.localStorage.removeItem(AUTH_TOKEN_STORAGE_KEY);
+                });
+                return d.promise;
+            };
+
+            return new Session();
+
         }
-        this.lastLoginUrl = null;
-        return url;
-      };
-
-      Session.prototype.clear = function () {
-        this.loggedIn = false;
-        this.name = null;
-        this.userName = null;
-        this.email = null;
-        this.id = null;
-        this.isAdmin = false;
-      };
-
-      Session.prototype.set = function (session) {
-        this.loggedIn = session.loggedIn;
-        this.name = session.name;
-        this.userName = session.userName;
-        this.email = session.email;
-        this.id = session.id;
-        this.isAdmin = session.isAdmin;
-      };
-
-      Session.prototype.login = function (result, token) {
-        this.set(result);
-        this.$delete = result.$delete;
-        if (token) {
-          $window.localStorage.setItem(AUTH_TOKEN_STORAGE_KEY, token);
+    ])
+    .service('AuthResource', [
+        '$resource',
+        'picnicUrls',
+        function ($resource, picnicUrls) {
+            return $resource(picnicUrls.auth, {}, {
+                'recoverPassword': {
+                    method: 'PUT',
+                    url: picnicUrls.auth + 'recoverpass'
+                },
+                'changePassword': {
+                    method: 'PUT',
+                    url: picnicUrls.auth + 'changepass'
+                }
+            });
         }
-      };
+    ])
+    .service('Alert', [
+        function () {
+            function Alert() {
+                var $this = this;
+                $this.messages = [];
 
-      Session.prototype.logout = function () {
-        var $this = this,
-            d = $q.defer();
-        $this.$delete(function (result) {
-          $this.clear();
-          d.resolve(result);
-          $window.localStorage.removeItem(AUTH_TOKEN_STORAGE_KEY);
-        });
-        return d.promise;
-      };
+                var addMessage = function (type, message) {
+                    $this.messages.push({
+                        message: message,
+                        type: type
+                    });
+                };
 
-      return new Session();
+                $this.dismiss = function (index) {
+                    $this.messages.splice(index, 1);
+                };
 
-    }
-  ])
-  .service('AuthResource', [
-    '$resource',
-    'picnicUrls',
-    function ($resource, picnicUrls) {
-      return $resource(picnicUrls.auth, {}, {
-        'recoverPassword': {
-          method: 'PUT',
-          url: picnicUrls.auth + 'recoverpass'
-        },
-        'changePassword': {
-          method: 'PUT',
-          url: picnicUrls.auth + 'changepass'
+                $this.dismissLast = function () {
+                    $this.messages.pop();
+                };
+
+                $this.success = addMessage.bind(null, "success");
+                $this.info = addMessage.bind(null, "info");
+                $this.warning = addMessage.bind(null, "warning");
+                $this.danger = addMessage.bind(null, "danger");
+            }
+
+            return new Alert();
+
         }
-      });
-    }
-  ])
-  .service('Alert', [
-    function () {
-      function Alert() {
-        var $this = this;
-        $this.messages = [];
-
-        var addMessage = function (type, message) {
-          $this.messages.push({
-            message: message,
-            type: type
-          });
-        };
-
-        $this.dismiss = function (index) {
-          $this.messages.splice(index, 1);
-        };
-
-        $this.dismissLast = function () {
-          $this.messages.pop();
-        };
-
-        $this.success = addMessage.bind(null, "success");
-        $this.info = addMessage.bind(null, "info");
-        $this.warning = addMessage.bind(null, "warning");
-        $this.danger = addMessage.bind(null, "danger");
-      }
-
-      return new Alert();
-
-    }
-  ]);
+    ]);
