52d383d2085e84c7b2e5845c65b90a3a3edc45e0
Cyrill Schumacher <cyrill@schumacher.fm>
2014-11-12 21:11:44 +1100

    [picnic/rd] brotzeit cron config

--- picnic/rd/src/app/brotzeit/BrotzeitController.js
+++ picnic/rd/src/app/brotzeit/BrotzeitController.js
@@ -1,9 +1,26 @@
 angular
-    .module('Wanderlust')
-    .controller('BrotzeitController', [
-        '$scope',
+  .module('Wanderlust')
+  .controller('BrotzeitController', [
+    '$scope',
+    '$modal',
+    function ($scope, $modal) {
+      $scope.showCronForm = function () {
+        alert('Use a popover');
+      };
+      $scope.openHelp = function () {
+        $modal.open({
+          templateUrl: 'partials/brotzeit/tpl/cronHelp.html',
+          controller: 'CronHelpController',
+          size: 'lg'
+        });
+      };
 
-        function ($scope) {
+    }])
+  // Please note that $modalInstance represents a modal window (instance) dependency.
+  // It is not the same as the $modal service used above.
+  .controller('CronHelpController', function ($scope, $modalInstance) {
 
-
-        }]);
+    $scope.close = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  });
